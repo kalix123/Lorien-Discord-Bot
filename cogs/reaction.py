baee46b8ef2e181ded861b7d1cf9551e819734f9
Kalix Zee <zeekalix@gmai.com>
2019-11-25 17:00:27 -0800

    update reactions to delete message

--- cogs/reaction.py
+++ cogs/reaction.py
@@ -14,19 +14,49 @@ class Reaction(commands.Cog):
             "thumbsup": "👍",
             "x": "❌"
         }
+        self.up_amount = 0
+        self.down_amount = 0
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        msg = reaction.message
+        message_id = str(msg.id)
+        # print(reaction)
+        if user.bot:
+            return
+        if reaction.emoji == self.emoji["thumbsup"]:
+            print("going up")
+            self.images[message_id]["thumbsup"] += 1
+        elif reaction.emoji == self.emoji["x"]:
+            self.images[message_id]["x"] += 1
+        if (self.images[message_id]["x"] >= 1) and (self.images[message_id]["x"] > self.images[message_id]["thumbsup"]):
+            await msg.delete()
+        # print(f"------------\nthumbsup: {self.images[message_id]['thumbsup']}\nx: {self.images[message_id]['x']}\n------------")
+    @commands.Cog.listener()
+    async def on_reaction_remove(self, reaction, user):
+        msg = reaction.message
+        message_id = str(msg.id)
+        if user.bot:
+            return
+        if reaction.emoji == self.emoji["thumbsup"]:
+            self.images[message_id]["thumbsup"] -= 1
+        elif reaction.emoji == self.emoji["x"]:
+            self.images[message_id]["x"] -= 1
+
+        # print(f"------------\nthumbsup: {self.images[message_id]['thumbsup']}\nx: {self.images[message_id]['x']}\n------------")
 
     @commands.Cog.listener()
     async def on_message(self,msg):
         if msg.attachments:
-            print(self.images)
+            self.down_amount = 0
+            self.up_amount = 0
             image_msg = msg
             image_id = str(image_msg.id)
             for choice in self.emoji:
                 react_emoji = self.emoji[choice]
-                self.images[image_id] = self.images.get(image_id, []) + [react_emoji]
                 await msg.add_reaction(emoji=react_emoji)
-                print(self.images)
 
+            self.images[image_id] = {'thumbsup': self.up_amount, 'x': self.down_amount}
 
 
 def setup(bot):
